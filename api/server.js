6adc95d6f2b8b8c0cc132d48b7c23bce98e1636b
johnkirtley <kirtleyj16@gmail.com>
2020-03-16 15:33:37 -0700

    Completed MVP and Stretch

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
-const express = require("express");
+const express = require('express');
 
-const db = require("../data/dbConfig.js");
+const db = require('../data/dbConfig.js');
+
+const AccountRouter = require('../accounts/account_router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+	res.status(200).json({ message: 'API Running' });
+});
 
 module.exports = server;
